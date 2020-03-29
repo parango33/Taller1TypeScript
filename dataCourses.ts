5e45aeec76be2b6eaa681f77aba4fbee6f9cad4b
Pablo Arango <p.arango@uniandes.edu.co>
2020-03-29 03:26:03 -0500

    Taller 1 TypeScript Pablo Arango

---
+++ dataCourses.ts
@@ -0,0 +1,8 @@
+
+import {Course} from './course.js';
+
+export const dataCourses = [
+    new Course("Desarrollo de Sotware","Rubby Casallas",3),
+    new Course("DALGO","Jorge Duitama",3),
+    new Course("Infraestructura computacional","Harold Castro",3)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,6 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de Sotware", "Rubby Casallas", 3),
+    new Course("DALGO", "Jorge Duitama", 3),
+    new Course("Infraestructura computacional", "Harold Castro", 3)
+];
